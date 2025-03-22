a68d46e179b23c0db6f95fdc5525dfb2cff1f503
Alexander Dickbauer <alexander@dickbauer.de>
2025-03-22 21:01:17 +0100

    created embedding base class

---
+++ embeddings/embedding_base.py
@@ -0,0 +1,19 @@
+import numpy as np
+import brotli
+from PIL import Image
+
+
+class Embedding_base:
+    def __init__(self, limit=-1):
+        self.ctr = 0
+        self.limit = limit
+
+    def __call__(self, img, executable) -> Image:
+        compress = self._compress(executable)
+        return self.function(img, compress)
+
+    def function(self, image, executable) -> Image:
+        pass
+
+    def _compress(self, executable) -> bytes:
+        pass
