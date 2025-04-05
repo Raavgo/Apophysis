24d0fc58a596dbb0099c934a91a59e7098fe7cbe
Alexander Dickbauer <alexander@dickbauer.de>
2025-04-05 21:06:42 +0200

    Implementend LSB

--- embeddings/lsb.py
+++ embeddings/lsb.py
@@ -1,7 +1,47 @@
 from PIL import Image
 
 from embeddings.embeddingbase import EmbeddingBase
+from typing import override
 
 class LSB(EmbeddingBase):
+    @override
     def function(self, image:Image, executable:bytes) -> Image:
-        pass
+        payload = executable.hex()
+        width, height = image.size
+        payload += chr(0)
+        payload_bits = ''.join([f"{ord(c):08b}" for c in payload])
+        data_index = 0
+
+        for y in range(height):
+            for x in range(width):
+                pixel = list(image.getpixel((x, y)))
+                for i in range(3):  # Iterate over R, G, B channels
+                    if data_index < len(payload_bits):
+                        pixel[i] = (pixel[i] & ~1) | int(payload_bits[data_index])
+                        data_index += 1
+                image.putpixel((x, y), tuple(pixel))
+                if data_index >= len(payload_bits):
+                    break
+            if data_index >= len(payload_bits):
+                break
+        return image
+
+    @override
+    def reverse_function(self,image:Image) -> bytes:
+        width, height = image.size
+        message_bits = []
+
+        for y in range(height):
+            for x in range(width):
+                pixel = list(image.getpixel((x, y)))
+                for i in range(3):
+                    message_bits.append(str(pixel[i] & 1))
+
+        message = ''.join(message_bits)
+        chars = [message[i:i + 8] for i in range(0, len(message), 8)]
+        decoded_message = ''.join([chr(int(char, 2)) for char in chars]).split(chr(0), 1)[0]
+        return bytes.fromhex(decoded_message)
+
+    @override
+    def __set_limit_function(self, dims) -> None:
+        self.limit = (dims[0] * dims[1] * 4) // 8
